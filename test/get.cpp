780574cb83240e32997f8cb0b4790c0d7b94480d
NewYdd <852732248@qq.com>
2017-03-13 08:46:31 +0800

    fist

---
+++ test/get.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"lib_io.h"
+
+int main(int argc,char *argv[])
+{
+	
+   
+    char *topo[MAX_EDGE_NUM];
+    int line_num;
+
+    char *topo_file = argv[1];
+
+    line_num = read_file(topo, MAX_EDGE_NUM, topo_file);
+
+    printf("line num is :%d \n", line_num);
+    if (line_num == 0)
+    {
+        printf("Please input valid topo file.\n");
+        return -1;
+    }
+
+   
+	return 0;
+}
+
+
